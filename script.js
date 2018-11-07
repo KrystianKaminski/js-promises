f2e05c25bb65d1b12a65565a5278a445c119a152
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-07 12:02:40 +0100

    Fetch part3

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-// function that is passed into then() is called when promise will be resolved
-// with promise resolution(value)
-
-// in fetch promise is resolving into Response object
 
 fetch('https://randomuser.me/api')
-    .then(response => console.log(response))
+    .then(response => response.json())
 
-// when data will be fetched promise is resolved
-// and function passed to  then is called with 
-// resolved data as an argument
+// We call .json() on resolved response object
+// Because we want to parse stream of bytes (body)
+// into JS object (in most cases object)
+
+    .then(data => console.log(data.results[0].name.first))
+
