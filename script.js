a182259ce9c0884d4fc72331ec70344ec8201c2b
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-07 12:07:37 +0100

    Fetch part4

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 
-fetch('https://randomuser.me/api')
+fetch('https://randomuser.me/api/?results=10')
     .then(response => response.json())
 
 // We call .json() on resolved response object
 // Because we want to parse stream of bytes (body)
 // into JS object (in most cases object)
 
-    .then(data => console.log(data.results[0].name.first))
+    .then(data => {
+        for (let i = 0; i < data.results.length; i++) {
+            console.log(data.results[i].name.first)
+        }
+    })
 
