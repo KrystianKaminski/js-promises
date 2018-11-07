fbca83a351ac4f5d8b272f26d3e7b164bcf3098e
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-07 12:16:25 +0100

    Fetch and render in html

--- script.js
+++ script.js
@@ -7,8 +7,16 @@ fetch('https://randomuser.me/api/?results=10')
 // into JS object (in most cases object)
 
     .then(data => {
+        const arr = []
         for (let i = 0; i < data.results.length; i++) {
-            console.log(data.results[i].name.first)
+            arr.push(data.results[i].name.first)
+        }
+        console.log(arr)
+
+        for (let a = 0; a < arr.length; a++) {
+            const div = document.createElement('div')
+            div.innerHTML = arr[a]
+            document.body.appendChild(div)
         }
     })
 
