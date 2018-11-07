dfa370d6289146b2f0c50b0f495e0632a44886ad
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-07 14:05:17 +0100

    Display email on click

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 class UserList {
-    constructor(container) {
+    constructor(container, id) {
         this.users = []
         this.container = container
+        this.id = id
+        this.init()
+    }
+
+    init() {
+        this.render()
+        this.fetchUsers()
+    }
+
+    fetchUsers() {
+        fetch(`https://randomuser.me/api/?results=${this.id}`)
+            .then(response =>
+                response.json())
+            .then(data => {
+                this.users = (data.results);
+                this.render()
+            })
     }
     render() {
         this.container.innerHTML = ''
@@ -10,9 +27,15 @@ class UserList {
 
         this.users.forEach( user => {
             const li = document.createElement('li')
-            li.innerText = `${name.first} ${name.last}`
+            li.innerText = `${user.name.first} ${user.name.last}`
+            list.appendChild(li)
+            li.addEventListener('click', () => {
+                alert(`${user.email}`)
+            })
         })
 
-        this.container.appendChild(ul)
+        this.container.appendChild(list)
     }
 }
+
+const list = new UserList(document.body, 21)
