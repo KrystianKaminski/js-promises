1d1c5fbf946f7daa82a019d6d9078ec4a0df1d39
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-08 13:56:23 +0100

    Promise.all

--- promise-all/script.js
+++ promise-all/script.js
@@ -2,28 +2,13 @@ const displayAllUsersEmail = () => {
     fetch('./data/users.json')
       .then(response => response.json())
       .then(users => {
-        let promisesResolved = 0
-        const numberOfUsers = users.length
-        const fetchArray = []
-  
-        users.forEach(user => {
-          fetchArray.push(
+        const fetchArray = users.map(user => (
             fetch(`./data/users/${user.uid}.json`)
               .then(response => response.json())
-              .then(data => {
-                promisesResolved++
+        ))
   
-                if (numberOfUsers === promisesResolved) {
-                  fetchArray.forEach(surlyResolvedPromise => {
-                    // here we ca be sure that promise
-                    surlyResolvedPromise.then(console.log)
-                  })
-                }
-  
-                return data
-              })
-          )
-        })
+        Promise.all(fetchArray)
+            .then(console.log)
       })
   }
   
